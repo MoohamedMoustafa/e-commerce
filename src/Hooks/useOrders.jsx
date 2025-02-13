b6a4cd2d40f6aaec97b3a89184c724c006490154
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2025-02-13 16:08:04 +0200

    Add dynamic user ID support for orders fetching

--- src/Hooks/useOrders.jsx
+++ src/Hooks/useOrders.jsx
@@ -1,22 +1,35 @@
-import { useQuery } from '@tanstack/react-query';
-import axios from 'axios'
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import { useEffect, useState } from "react";
 
 export default function useOrders() {
-    const userId = '678a78a5a036763e8b6db5a0';
-    async function getOrders() {
-        try {
-            const response = await axios.get(`https://ecommerce.routemisr.com/api/v1/orders/user/${userId}`)
-            return response;
-        } catch (err) {
-            console.error("error from Orders: ", err.message);
-            return err;
-        }
+  const [userId, setUserId] = useState(null);
+  useEffect(() => {
+    const token = localStorage.getItem("UserToken");
+    if (token) {
+      const decoded = jwtDecode(token);
+      console.log("decoded",JSON.stringify(decoded.id));
+      setUserId(decoded.id);
     }
-    const ordersObject = useQuery({
-        queryKey : ['orders'],
-        queryFn : getOrders,
-        select: (data) => data.data
-    })
-    return ordersObject;
+  }, []);
+
+  async function getOrders() {
+    try {
+      const response = await axios.get(
+        `https://ecommerce.routemisr.com/api/v1/orders/user/${userId}`
+      );
+      return response;
+    } catch (err) {
+      console.error("error from Orders: ", err.message);
+      throw err;
+    }
+  }
+  const ordersObject = useQuery({
+    queryKey: ["orders", userId],
+    queryFn: getOrders,
+    select: (data) => data.data,
+    enabled: Boolean(userId),
+  });
+  return ordersObject;
 }
