bca9cb1ff7dd83a5d91893bf38b1b22a2407bf41
MoohamedMoustafa <mohamedmustafa.pharma@gmail.com>
2025-02-13 19:56:56 +0200

    updated ProductDetails component to use ProdctCard

--- src/components/RelatedProducts/RelatedProducts.jsx
+++ src/components/RelatedProducts/RelatedProducts.jsx
@@ -3,6 +3,7 @@ import style from "./RelatedProducts.module.css";
 import { Link } from "react-router-dom";
 import useProducts from "./../../Hooks/useProducts";
 import { ClipLoader } from 'react-spinners';
+import ProductCard from "../ProductCard/ProductCard";
 
 export default function RelatedProducts({ category }) {
   const [relatedProducts, setRelatedProducts] = useState([]); // new
@@ -48,43 +49,11 @@ export default function RelatedProducts({ category }) {
     <>
       {/* related poroducts  */}
       <div className="row gap-y-8 my-10">
-        {relatedProducts.length > 0 ? (
+        {
           relatedProducts.map((product) => (
-            <div key={product.id} className="md:w-1/6 px-2 ">
-              <div className="product px-2 py-1">
-                <Link
-                  to={`/productdetails/${product.id}/${product.category.name}`}
-                >
-                  <img
-                    src={product.imageCover}
-                    alt={product.title}
-                    className="w-full"
-                  />
-                  <h3 className=" text-emerald-600 mb-1">
-                    {product.category.name}
-                  </h3>
-                  <h3 className="font-semibold mb-1">
-                    {product.title.split(" ").slice(0, 2).join(" ")}
-                  </h3>
-                  <div className="flex justify-around mb-2">
-                    <span>{product.price} EGP</span>
-                    <span>
-                      <i className="fas fa-star text-yellow-400"></i>
-                      {product.ratingsAverage}
-                    </span>
-                  </div>
-                </Link>
-                <button className="btn">Add To Cart</button>
-              </div>
-            </div>
+            <ProductCard product={product} key={product.id} />
           ))
-        ) : (
-          <>
-            <div className="flex justify-center items-center min-h-screen w-full">
-              <span className="loader"></span>
-            </div>
-          </>
-        )}
+        }
       </div>
     </>
   );
